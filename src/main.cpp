f4bc7eb2721c52bbe485e371c834e449e5838882
Mark Zieg <mark@zieg.com>
2022-05-11 19:19:40 -0400

    renaming artifacts to align with ENLIGHTEN demo plugin

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,7 @@
 using std::string;
 using std::list;
 
-const char* VERSION = "RamanSpectralIdentify-1.0.1";
+const char* VERSION = "RamanIDAlgo-1.0.1";
 
 //! holds parsed command-line options controlling runtime behavior
 struct Options
